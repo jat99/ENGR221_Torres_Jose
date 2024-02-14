661dcb3f4b83330a042c93ad3d7ea80c91f415fa
Jose Torres <devjt99@gmail.com>
2024-02-13 19:45:08 -0800

    New Insertion Sort
    Implement Insertion Sort another way and also made some comments

--- Lab3/sortingFunctions.py
+++ Lab3/sortingFunctions.py
@@ -6,20 +6,31 @@ Description: Implementation of sorting algorithms.
 
 import time, random
 
-# Implementation of insertionSort algorithm
+# Implementation of insertionSort algorithm - swapping elements
 def insertionSort(list_to_sort: list) -> list:
+    #Loop array
     for index in range(len(list_to_sort)):
         j = index
-        while j > 0 and list_to_sort[j-1] > list_to_sort[j]:
+        #checek j in bounds and the previous element is smaller than currrent
+        while j > 0 and list_to_sort[j-1] > list_to_sort[j]: 
             swap(list_to_sort, j, j-1)
-            j = j - 1
+    return list_to_sort
+
+# Second Implementation of insertion sort using shift
+def insertionSort_1(list_to_sort: list) -> list:
+    for index in range(1, len(list_to_sort)): #Start at 1 since first element is always in "Correct Place"
+        value = list_to_sort[index] #Store the value of current element
+        j = index - 1 #Store the previous element
+        while (value < list_to_sort[j] and j >= 0):
+            list_to_sort[j+1] = list_to_sort[j] #Shift elements to the right
+            j = j - 1 #Decrement j to check values more left
+        list_to_sort[j+1] = value #Once you have shifted all the way insert the value back
     return list_to_sort
 
 # Implementation of bubbleSort algorithm
 def bubbleSort(list_to_sort:list) -> list:
-    ### Add your bubbleSort implementation here
     for i in range(len(list_to_sort) - 1):
-        for j in range(len(list_to_sort) - 1 - i):
+        for j in range(len(list_to_sort) - 1 - i): #Decrement range every time previous loop increments
             if list_to_sort[j] > list_to_sort[j + 1]:
                 swap(list_to_sort,j, j+1)
     return list_to_sort
@@ -49,5 +60,6 @@ def getRuntime(function_to_run, list_length) -> float:
 
 
 if __name__ == '__main__':
-    print(getRuntime(bubbleSort, 10000))
-    pass
+   a = insertionSort_1([8,4,1,2])
+   print(a)
+pass
